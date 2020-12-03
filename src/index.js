15f76886b559581e2c2850fea94b318420cd844e
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-02 18:39:21 -0800

    Fixed and reduced imports in index, moved store

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-import {BrowserRouter as Router} from 'react-router-dom'
-import userReducer from './reducers/userReducer'
-
 import App from './App';
+import { Provider } from 'react-redux'
+import store from './store.js'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-let store = createStore(userReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('root'));
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
+    document.getElementById('root'));
