5719be93405ce85544f7c7890e6521c6a2695964
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-02 19:20:27 -0800

    set up userReducer in combiner reducers. will export user array with key Users

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
+import userReducer from './reducers/userReducer.js'
 
 const reducer = combineReducers({
-
+    users: userReducer
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
