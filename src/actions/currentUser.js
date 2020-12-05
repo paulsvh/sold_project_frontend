d165a2bf49b86bff1ee216de9d45c3ab80a9f51f
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-05 15:14:12 -0800

    add action and dispatch to clear login form state when a user logs in. This will ensure that when going to log in, the form will be blank.

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,3 +1,5 @@
+import {resetLoginForm} from './loginForm.js'
+
 export const setCurrentUser = user => {
     return {
         type: 'SET_CURRENT_USER',
@@ -22,6 +24,7 @@ export const login = credentials => {
                 alert(user.error)
             } else {
                 dispatch(setCurrentUser(user))
+                dispatch(resetLoginForm())
             }
         })
         .catch(console.log)
