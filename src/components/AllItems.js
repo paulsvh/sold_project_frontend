872ee36c888a2bad8edff66a0867e5ea9aff20f8
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-20 19:33:46 -0800

    add searchbar and search functionality to browse

--- src/components/AllItems.js
+++ src/components/AllItems.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 
 
+
 const AllItems = props => {
-    const allButMyItems = props.items.filter(item => item.user.id != props.user.id)
-    const itemCards = allButMyItems.map(item => <Link to={`/browse/${item.id}`} key={item.id}>{item.title}<br/><br/></Link>) 
+    const [search, setSearch] = useState('')
+    const allButMyItems = props.items.filter(item => item.user.id !== props.user.id)
+    const filteredItems = allButMyItems.filter(item => {
+        return item.title.toLowerCase().includes(search.toLowerCase())
+    }) 
+    const itemCards = filteredItems.map(item => <Link to={`/browse/${item.id}`} key={item.id}>{item.title}<br/><br/></Link>)
+
     return(
-        itemCards
+        <div>
+            <input type="text" placeholder="Search For Something" onChange={e => setSearch(e.target.value)}/><br/><br/>
+            {itemCards}
+        </div>
+        
     )
 }
 
